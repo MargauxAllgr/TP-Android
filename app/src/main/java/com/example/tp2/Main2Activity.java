5e273aa5a70f30c00039e2acaa04a819b0a21070
lalamefine <l.triboulin@gmail.com>
2019-11-06 16:03:12 +0100

    Etudiant Fix

--- app/src/main/java/com/example/tp2/Main2Activity.java
+++ app/src/main/java/com/example/tp2/Main2Activity.java
@@ -1,19 +1,15 @@
 package com.example.tp2;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import modele.Etudiant;
 
